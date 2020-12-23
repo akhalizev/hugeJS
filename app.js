2990c9ea70fa3b815252ae4acf4e13ad8ae5701d
akhalizev <ahalizev@gmail.com>
2020-12-22 22:38:36 -0600

    external js

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.btn').forEach(
+            (item) => {
+                item.addEventListener('click', ()=> {
+                    alert('this is cool!')
+                })
+            }
+        )
