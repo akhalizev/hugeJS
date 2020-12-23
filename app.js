faa4191bcfc08fc413804cc6d7629e513bad0c1a
akhalizev <ahalizev@gmail.com>
2020-12-23 11:44:35 -0600

    array-func-for

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-document.querySelectorAll('.btn').forEach(
-            (item) => {
-                item.addEventListener('click', ()=> {
-                    alert('this is cool!')
-                })
-            }
-        )
+// const bob = 'Bob';
+// const susy = 'Susy';
+
+// function greet(name, second) {
+//     console.log('hello there, ' + name);    
+// }
+
+// greet('matt', susy);
+
+const gas = [20, 40, 100, 30];
+const food = [10, 40, 50];
+
+function calculateTotal(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
+
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+
+console.log({
+    gas: gasTotal,
+    food: foodTotal
+});
