ac8e12dd249f565e84a15766d924297d75b21f73
akhalizev <ahalizev@gmail.com>
2020-12-24 17:02:17 -0600

    forEarch with callback

--- app.js
+++ app.js
@@ -1,54 +1,17 @@
-// function greetMorning(name) {
-//     const myName = 'john';
-//     console.log(`Good morning ${name}, my name is ${myName}`);
-// }
+//for each
 
-// function greetAfternoon(name) {
-//     const myName = 'john';
-//     console.log(`Good afternoon ${name}, my name is ${myName}`);
-// }
-function morning(name) {
-    console.log();
-    return `Good morning ${name.toUpperCase()}`;
-}
+const people = [
+    { name: 'bob', age: 20, position: 'developer' },
+    { name: 'peter', age: 30, position: 'designer' },
+    { name: 'sus', age: 25, position: 'director' },
+];
 
-function greet(name, cb) {
-    //cb(name);
-    const myName = 'john';
-    console.log(`${cb(name)}, my name is ${myName}`);
-}
+function showPerson(person) {
+    console.log(person.position.toUpperCase());
+};
 
-greet('peter', morning);
-greet('bob', morning);
-// const bob = 'Bob';
-// const susy = 'Susy';
+//people.forEach(showPerson);
 
-// function greet(name, second) {
-//     console.log('hello there, ' + name);    
-// }
-
-// greet('matt', susy);
-
-// const gas = [20, 40, 100, 30];
-// const food = [10, 40, 50];
-
-// function calculateTotal(arr) {
-//     let total = 0;
-//     for (let i = 0; i < arr.length; i++){
-//         total += arr[i];
-//     }
-//     if (total > 100) {
-//         console.log(`You're spending too much!`);
-//         return total;
-//     }
-//     console.log(`You are in a good situation`);
-//     return total;
-// }
-
-// const gasTotal = calculateTotal(gas);
-// const foodTotal = calculateTotal(food);
-
-// console.log({
-//     gas: gasTotal,
-//     food: foodTotal
-// });
+people.forEach((item) => {
+    console.log(item.position.toUpperCase());
+})
