af9ddce8e8c4603e848ecb71c89b18b8c33a3160
akhalizev <ahalizev@gmail.com>
2020-12-24 16:05:27 -0600

    simple callback func

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// function greetMorning(name) {
+//     const myName = 'john';
+//     console.log(`Good morning ${name}, my name is ${myName}`);
+// }
+
+// function greetAfternoon(name) {
+//     const myName = 'john';
+//     console.log(`Good afternoon ${name}, my name is ${myName}`);
+// }
+function morning(name) {
+    console.log();
+    return `Good morning ${name.toUpperCase()}`;
+}
+
+function greet(name, cb) {
+    //cb(name);
+    const myName = 'john';
+    console.log(`${cb(name)}, my name is ${myName}`);
+}
+
+greet('peter', morning);
+greet('bob', morning);
 // const bob = 'Bob';
 // const susy = 'Susy';
 
@@ -7,26 +29,26 @@
 
 // greet('matt', susy);
 
-const gas = [20, 40, 100, 30];
-const food = [10, 40, 50];
+// const gas = [20, 40, 100, 30];
+// const food = [10, 40, 50];
 
-function calculateTotal(arr) {
-    let total = 0;
-    for (let i = 0; i < arr.length; i++){
-        total += arr[i];
-    }
-    if (total > 100) {
-        console.log(`You're spending too much!`);
-        return total;
-    }
-    console.log(`You are in a good situation`);
-    return total;
-}
+// function calculateTotal(arr) {
+//     let total = 0;
+//     for (let i = 0; i < arr.length; i++){
+//         total += arr[i];
+//     }
+//     if (total > 100) {
+//         console.log(`You're spending too much!`);
+//         return total;
+//     }
+//     console.log(`You are in a good situation`);
+//     return total;
+// }
 
-const gasTotal = calculateTotal(gas);
-const foodTotal = calculateTotal(food);
+// const gasTotal = calculateTotal(gas);
+// const foodTotal = calculateTotal(food);
 
-console.log({
-    gas: gasTotal,
-    food: foodTotal
-});
+// console.log({
+//     gas: gasTotal,
+//     food: foodTotal
+// });
