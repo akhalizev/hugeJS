a9b4bff440c7c9d39c5447e74fe2e817c6aa2b85
akhalizev <ahalizev@gmail.com>
2020-12-24 18:06:00 -0600

    filter

--- app.js
+++ app.js
@@ -6,35 +6,48 @@ const people = [
     { name: 'sus', age: 25, position: 'director' },
 ];
 
-function showPerson(person) {
-    console.log(person.position.toUpperCase());
-};
+// function showPerson(person) {
+//     console.log(person.position.toUpperCase());
+// };
 
 //people.forEach(showPerson);
 
-people.forEach((item) => {
-    console.log(item.position.toUpperCase());
-})
+// people.forEach((item) => {
+//     console.log(item.position.toUpperCase());
+// })
 
 //map
 
-const ages = people.map(function (person) {
-    return person.age;
+// const ages = people.map(function (person) {
+//     return person.age;
+// });
+
+// const newPeople = people.map((person) => {
+//     return {
+//         firstName: person.name.toUpperCase(),
+//         oldAge: person.age + 20,
+//     }
+// });
+
+// const names = people.map((person) => {
+//     return `<h1>${person.name}</h1>`
+// })
+
+// console.log(ages);
+// console.log(newPeople);
+
+// document.body.innerHTML = names.join('');
+// console.log(names);
+
+const youngPeople = people.filter((person) => {
+    return person.age < 26;
 });
 
-const newPeople = people.map((person) => {
-    return {
-        firstName: person.name.toUpperCase(),
-        oldAge: person.age + 20,
-    }
+console.log(youngPeople);
+document.body.innerHTML = youngPeople;//
+
+const developers = people.filter((person) => {
+    return person.position === 'developer';
 });
 
-const names = people.map((person) => {
-    return `<h1>${person.name}</h1>`
-})
-
-console.log(ages);
-console.log(newPeople);
-
-document.body.innerHTML = names.join('');
-console.log(names);
+console.log(developers);
