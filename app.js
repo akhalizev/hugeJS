a1bd7ae1a55a9e9a19248a245ee779194ec3f438
akhalizev <ahalizev@gmail.com>
2020-12-24 18:52:02 -0600

    find

--- app.js
+++ app.js
@@ -1,54 +1,15 @@
 //for each
 
 const people = [
-    { name: 'bob', age: 20, position: 'developer' },
-    { name: 'peter', age: 30, position: 'designer' },
-    { name: 'sus', age: 25, position: 'director' },
+    { name: 'bob', age: 20, position: 'developer', id: 0 },
+    { name: 'peter', age: 30, position: 'designer', id: 1 },
+    { name: 'sus', age: 25, position: 'director', id: 3 },
 ];
 
-// function showPerson(person) {
-//     console.log(person.position.toUpperCase());
-// };
-
-//people.forEach(showPerson);
-
-// people.forEach((item) => {
-//     console.log(item.position.toUpperCase());
-// })
-
-//map
-
-// const ages = people.map(function (person) {
-//     return person.age;
-// });
-
-// const newPeople = people.map((person) => {
-//     return {
-//         firstName: person.name.toUpperCase(),
-//         oldAge: person.age + 20,
-//     }
-// });
-
-// const names = people.map((person) => {
-//     return `<h1>${person.name}</h1>`
-// })
-
-// console.log(ages);
-// console.log(newPeople);
-
-// document.body.innerHTML = names.join('');
-// console.log(names);
-
-const youngPeople = people.filter((person) => {
-    return person.age < 26;
-});
+const person = people.find((person) => {
+    return person.id === 3;
+})
 
 
-console.log(youngPeople);
-//document.body.innerHTML = `<h2>${young}</h2>`;//
 
-const developers = people.filter((person) => {
-    return person.position === 'developer';
-});
-
-console.log(developers);
+document.body.innerHTML = `<h4>${person.name}</h4>`
